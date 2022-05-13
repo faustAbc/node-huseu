5dfa39388e650e2ac2eede97324c2342ae05474c
Alex Gusev <aliaksandr.huseu@burberry.com>
2022-05-13 11:56:28 +0400

    chore: remove memory calculations

--- src/task-2/index.ts
+++ src/task-2/index.ts
@@ -6,8 +6,6 @@ import { promisify } from "util";
 
 const pipeline = promisify(stream.pipeline);
 
-const initialMemoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-
 const inputPath = path.join(__dirname, "csv/test.csv");
 const outputDir = path.join(__dirname, "json");
 const outputPath = path.join(outputDir, `${new Date().getTime()}.txt`);
@@ -16,18 +14,12 @@ if (!existsSync(outputDir)) {
   mkdirSync(outputDir);
 }
 
-const errorHandler = (error: Error) => console.error.bind(console, error);
+const errorHandler = console.error;
 
 const readCsvStream = createReadStream(inputPath).on("error", errorHandler);
 const writeJsonStream = createWriteStream(outputPath).on("error", errorHandler);
-const closeHandler = () => {
-  const finalMemoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-  console.log(
-    "Used memory: " + (finalMemoryUsed - initialMemoryUsed).toFixed(2) + " MB"
-  );
-};
 
 pipeline(
   csv().fromStream(readCsvStream),
-  writeJsonStream.on("error", errorHandler).on("close", closeHandler)
+  writeJsonStream.on("error", errorHandler)
 );
